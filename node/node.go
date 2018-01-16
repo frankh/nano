4650def64974369832d08ca89b8617eda3ef3dcd
Frank Hamand <frankhamand@gmail.com>
2018-01-16 18:14:03 +0000

    Add more error handling

--- node/node.go
+++ node/node.go
@@ -99,25 +99,51 @@ func (m *MessageCommon) WriteCommon(buf *bytes.Buffer) error {
 }
 
 func (m *MessageHeader) WriteHeader(buf *bytes.Buffer) error {
-	buf.WriteByte(m.MagicNumber[0])
-	buf.WriteByte(m.MagicNumber[1])
-	buf.WriteByte(m.VersionMax)
-	buf.WriteByte(m.VersionUsing)
-	buf.WriteByte(m.VersionMin)
-	buf.WriteByte(m.MessageType)
-	buf.WriteByte(m.Extensions)
-	buf.WriteByte(m.BlockType)
+	var errs []error
+	errs = append(errs,
+		buf.WriteByte(m.MagicNumber[0]),
+		buf.WriteByte(m.MagicNumber[1]),
+		buf.WriteByte(m.VersionMax),
+		buf.WriteByte(m.VersionUsing),
+		buf.WriteByte(m.VersionMin),
+		buf.WriteByte(m.MessageType),
+		buf.WriteByte(m.Extensions),
+		buf.WriteByte(m.BlockType),
+	)
+
+	for _, err := range errs {
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (m *MessageHeader) ReadHeader(buf *bytes.Buffer) error {
-	m.MagicNumber[0], _ = buf.ReadByte()
-	m.MagicNumber[1], _ = buf.ReadByte()
-	m.VersionMax, _ = buf.ReadByte()
-	m.VersionUsing, _ = buf.ReadByte()
-	m.VersionMin, _ = buf.ReadByte()
-	m.MessageType, _ = buf.ReadByte()
-	m.Extensions, _ = buf.ReadByte()
-	m.BlockType, _ = buf.ReadByte()
+	var errs []error
+	var err error
+	// I really hate go error handling sometimes
+	m.MagicNumber[0], err = buf.ReadByte()
+	errs = append(errs, err)
+	m.MagicNumber[1], err = buf.ReadByte()
+	errs = append(errs, err)
+	m.VersionMax, err = buf.ReadByte()
+	errs = append(errs, err)
+	m.VersionUsing, err = buf.ReadByte()
+	errs = append(errs, err)
+	m.VersionMin, err = buf.ReadByte()
+	errs = append(errs, err)
+	m.MessageType, err = buf.ReadByte()
+	errs = append(errs, err)
+	m.Extensions, err = buf.ReadByte()
+	errs = append(errs, err)
+	m.BlockType, err = buf.ReadByte()
+	errs = append(errs, err)
+
+	for _, err := range errs {
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
